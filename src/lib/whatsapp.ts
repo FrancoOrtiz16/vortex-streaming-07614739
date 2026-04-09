91c177e9a03b4765ddc622e9de9381cef45a6326
FrancoOrtiz16 <fg30606212@gmail.com>
2026-04-09 17:27:42 +0000

    Mejoras

--- src/lib/whatsapp.ts
+++ src/lib/whatsapp.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMBER = '854242644553';
+export const WHATSAPP_NUMBER = '584242644553';
 
 export const getWhatsAppUrl = (message: string) => `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`;
 
